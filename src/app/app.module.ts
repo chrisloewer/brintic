4ba810d3da956c41530d7836fd78c2767b6f9920
chris <chris.loewer@gmail.com>
2018-04-09 18:02:42 -0500

    Initialize PageControl and HomePage components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import {RouterModule, Routes} from '@angular/router';
+import { PageControlComponent } from './components/page-control/page-control.component';
 
+const appRoutes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: '', component: HomePageComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    PageControlComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [PageControlComponent]
 })
 export class AppModule { }
---
+++ src/app/components/page-control/page-control.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './page-control.component.html',
+  styleUrls: ['./page-control.component.scss']
+})
+export class PageControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
