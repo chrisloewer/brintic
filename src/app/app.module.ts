ceaf9b1d0ebfa1e4734c04739042d65e26227317
chris <chris.loewer@gmail.com>
2018-04-14 15:59:25 -0500

    Add admin page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,19 @@ import { NgModule } from '@angular/core';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { RouterModule, Routes } from '@angular/router';
 import { PageControlComponent } from './components/page-control/page-control.component';
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomePageComponent },
+  { path: 'admin', component: AdminPageComponent },
   { path: '', component: HomePageComponent }
 ];
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    PageControlComponent
+    PageControlComponent,
+    AdminPageComponent
   ],
   imports: [
     BrowserModule,
