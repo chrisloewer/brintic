c8e0cce688739d148250f920a2f5ed67ebb48304
chris <chris.loewer@gmail.com>
2018-04-14 11:56:45 -0500

    Add fade-in animation to content!

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -7,8 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  scrollY = 0;
+
+  constructor(
+    private elem: ElementRef
+  ) { }
 
   ngOnInit() { }
 
+  scrollHandler(event): void {
+    // Get how many px the main container has scrolled
+    // Add height of the container to count from the bottom of the screen, minus buffer
+    this.scrollY = event.target.scrollTop + event.target.offsetHeight - 80;
+  }
+
+  isVisible(elementId): boolean {
+    const domElem = this.elem.nativeElement.querySelector(elementId);
+
+    // For nested elements, add parent distance to get true distance to top
+    const offset = domElem.offsetTop + domElem.parentElement.offsetTop;
+    if (this.scrollY >= offset) {
+      return true;
+    }
+    return false;
+  }
 }
