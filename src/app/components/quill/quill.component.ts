d3bac98c6013c0a23b10d5cb3cbcf446838da660
chris <chris.loewer@gmail.com>
2018-04-25 22:12:48 -0500

    Make certain quill styles universal to use them in views.

--- src/app/components/quill/quill.component.ts
+++ src/app/components/quill/quill.component.ts
@@ -18,20 +18,17 @@ export class QuillComponent implements OnInit {
     toolbar: [
       [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
       [{ 'align': [] }],
-      ['bold', 'italic', 'underline'],        // toggled buttons
+      ['bold', 'italic', 'underline'],                  // toggled buttons
 
-      [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-
-      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+      [{ 'size': ['small', false, 'large', 'huge'] }],  // size drop-down
 
       ['blockquote', 'code-block'],
 
       [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-      [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
 
       ['clean'],                                         // remove formatting button
 
-      ['link', 'image', 'video']                         // link and image, video
+      ['link', 'image']                                 // link and image, video
     ],
     clipboard: {
       matchVisual: false
