1538761f4bd11f9e8d8f3bc90186ef9e999f3e51
chris <chris.loewer@gmail.com>
2018-05-12 12:28:49 -0500

    Load features page content from DB and be able to edit page.

--- src/app/components/features-page/features-page.component.ts
+++ src/app/components/features-page/features-page.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { PostService } from '../../services/post.service';
+import { Post } from '../../classes/post';
 
 @Component({
   selector: 'app-features-page',
   templateUrl: './features-page.component.html',
-  styleUrls: ['./features-page.component.scss']
+  styleUrls: ['./features-page.component.scss'],
+  providers: [PostService]
 })
 export class FeaturesPageComponent implements OnInit {
 
-  constructor() { }
+  @Input() post: Post;
+
+  constructor(
+    private elem: ElementRef,
+    private postService: PostService,
+  ) { }
 
   ngOnInit() {
+    // if Post not passed in by edit component, load based on url
+    if (!this.post) {
+      this.getPost();
+    }
+  }
+
+  getPost(): void {
+    this.postService.getPost('features')
+      .subscribe(
+        (p) => this.post = p,
+        (err) => console.warn(err)
+      );
+  }
+
+  getContent(contentBlockId): String {
+    try {
+      return this.post.contentBlocks.filter(function (block) {
+        return block.id === contentBlockId;
+      })[0].content;
+    } catch (e) { }
+    return '';
+  }
+
+  // Only return url if path is specified
+  // This stops invalid network requests while the path is being loaded from api call
+  getUrl(path: string): string {
+    if (path) {
+      return 'url(' + path + ')';
+    }
+    return null;
   }
 
 }
